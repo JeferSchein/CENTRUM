5675fe974c84f2e2a1f2e874e60dc6a5cf1b5572
Jeferson Schein <schein.jefer@gmail.com>
2014-05-07 09:56:08 -0300

    metodo interpreta adicionado a classe

--- Interpretador.java
+++ Interpretador.java
@@ -1,4 +1,13 @@
 class Interpretador{
-
+	private String cmandos[];
+	
+	public void interpreta(String lin[]){
+		this.comandos = l;
+		
+		for(int i = 0; i < this.comandos.length; i++){
+			
+			
+		}
+	}
 
 }
