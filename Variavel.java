936008546d831d989fb722ff55e70a78ec87fef5
Jeferson Schein <schein.jefer@gmail.com>
2014-05-08 08:31:26 -0300

    seter e geter para variaveis

--- Variavel.java
+++ Variavel.java
@@ -1,7 +1,22 @@
 calss Variavel{
-	private string nome;
-	private string tipo;
+	private String nome;
+	private String valor;
 	
-
+	public String getNome(){
+		return this.nome;
+	}
+	
+	public String getValor(){
+		return this.valor;
+	}
+	
+	public void setNome(String n){
+		this.nome = n;
+	}
+	
+	public void setTipo(String v){
+		this.valor = v;
+	}
+	
 
 }
