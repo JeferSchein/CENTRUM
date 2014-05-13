7a42f9ab08703183b60aa15f05e69a76f30286ff
SmashTester <felipe_rwitezak@hotmail.com>
2014-05-13 19:12:53 -0300

    Update Centrum.java

--- fonte/Centrum.java
+++ fonte/Centrum.java
@@ -1,31 +1,24 @@
 import java.util.Scanner;
 import java.io.File;
 
-class Centrum {
-	public static void main(String args[]) {
-		File arquivo;
-		Scanner scan;
-		Interpretador lin;
-		String comandos[] = new String[2000]; // arquivo pode ter, no máximo, 2000 linhas.
-        
-		try {
-			arquivo = new File(args[0]);
-			scan = new Scanner(arquivo);
-			lin = new Interpretador();
-            
-			int i = 0;
-			while(scan.hasNext()) {
-				comandos[i] = scan.nextLine();
-				i++;
-			}
-            
-			lin.interpreta(comandos);
-            
-		} catch (Exception e) {
-			System.out.println("Nao consegui ler o arquivo: " + (args.length > 0 ? args[0] : "(desconhecido)"));
+public class Centrum{
+
+	public static void main(String[] args) {
+		
+		// Se não há arquivo de entrada mostra a ajuda.
+		if (args.length !=1) {
 			System.out.println("Uso:");
-			System.out.println("    java Blah /caminho/para/arquivo.blah");
+			System.out.println("java Centrum /caminho/para/arquivo.cem");
+			return;
 		}
-        
+
+		// Lê o arquivo.
+		File arquivo = new File(args[0]);
+		Scanner conteudo = new Scanner(arquivo);
+
+		//Executa o interpretador.
+		Interpretador centrum  = new Interpretador();
+		centrum.interpreta(conteudo);
 	}
+
 }
