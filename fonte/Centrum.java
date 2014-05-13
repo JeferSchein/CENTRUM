7fd7de301bc4bd5104fa213f20b5e93050425635
SmashTester <felipe_rwitezak@hotmail.com>
2014-05-13 19:24:24 -0300

    Update Centrum.java

--- fonte/Centrum.java
+++ fonte/Centrum.java
@@ -4,21 +4,22 @@ import java.io.File;
 public class Centrum{
 
 	public static void main(String[] args) {
-		
-		// Se não há arquivo de entrada mostra a ajuda.
-		if (args.length !=1) {
-			System.out.println("Uso:");
-			System.out.println("java Centrum /caminho/para/arquivo.cem");
+		try{
+			// Lê o arquivo.
+			File arquivo = new File(args[0]);
+			Scanner conteudo = new Scanner(arquivo);
+
+		}catch (Exception e) {
+
+			// Mostra o uso e sair caso o arquivo seja inválido.
+			System.out.println("Nao e possivel ler o arquivo: " + (args.length > 0 ? args[0] : "(desconhecido)"));
+			System.out.println("Uso:\n    Java Centrum /caminho/para/arquivo.cem");
 			return;
 		}
 
-		// Lê o arquivo.
-		File arquivo = new File(args[0]);
-		Scanner conteudo = new Scanner(arquivo);
-
-		//Executa o interpretador.
-		Interpretador centrum  = new Interpretador();
+		Interpretador centrum = new Interpretador();
 		centrum.interpreta(conteudo);
+
 	}
 
 }
