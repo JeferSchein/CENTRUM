1f5b0fb80ff4258537ba7dceff4b165c252a4cb4
Ivairpuerari <ivaair@hotmail.com>
2014-05-17 09:35:06 -0300

    classe operador , para condição  de maior, menor e igual.

--- fonte/Lance.java
+++ fonte/Lance.java
@@ -1,18 +1,18 @@
 class Laco extends Fluxo{
-			
+
 	public boolean while(operador a,variavel b, int c){
-			if(a.condicao==o){
-				return public boolean logic igual(variavel b, int c );			
+			if(a.condicao==0){
+				return public boolean logic igual(variavel b, int c );
 			}
 			if(a.condicao==1){
 				return public boolean logic maior(variavel b, int c);
 			}
 			if(a.condicao==-1){
-				return public boolean logic menor(variavel b, int c);	
+				return public boolean logic menor(variavel b, int c);
 			}
-	}	
-		
-	
-	
-	
+	}
+
+
+
+
 }
