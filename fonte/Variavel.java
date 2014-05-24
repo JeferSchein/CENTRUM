b27cefbc6543df6419c5b367a463ad604c87637f
schein00 <schein.jefer@gmail.com>
2014-05-24 12:08:07 -0300

    atualizacao Variavel.java
    mudanca na declaracao de variavel

--- fonte/Variavel.java
+++ fonte/Variavel.java
@@ -1,30 +1,46 @@
 class Variavel{
 	private String nome;
+	private String valor;
 	
 	public Variavel(String n, int v){
-		public int valor;
-		valor = v;
+		valor = String.valueOf(v);
 		nome = n;
 	}
 	public Variavel(String n, boolean v){
-		public boolean valor;
-		valor = v;
+		valor = String.valueOf(v);
 		nome = n;
 	}
 	public Variavel(String n, String v){
-		public String valor;
 		valor = v;
 		nome = n;
 	}
 	public Variavel(String n, double v){
-		public valor;
-		valor = v;
+		valor = String.valueOf(v);
 		nome = n;
+	}
+
+	public void setValor(int v){
+		valor = String.valueOf(v);
+	}
+
+	public void setValor(boolean v){
+		valor = String.valueOf(v);
+	}
+
+	public void setValor(double v){
+		valor = String.valueOf(v);
+	}
+
+	public void setValor(String v){
+		valor = v;
 	}
 	
 	public String getNome(){
 		return this.nome;
 	}
-
+	
+		public String getValor(){
+		return this.valor;
+	}
 
 }
