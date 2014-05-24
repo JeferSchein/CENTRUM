cc624278105afeaf19318cea83f4be56ebbb95fe
Ivairpuerari <ivaair@hotmail.com>
2014-05-24 13:33:03 -0300

    correção erro na função boolean

--- fonte/Fluxo.java
+++ fonte/Fluxo.java
@@ -2,24 +2,24 @@ class Fluxo{
 
 	public boolean logico;
 	
-	public boolean logic igual(variavel b,int a){
-		if(b.valor == a){
+	public boolean logic_igual(variavel b,int a){
+		if(b.getVALOR() == a){
 		      return logico = TRUE;
 		}
 		else{
 		      return logico = FALSE;		
 		}
 	}		
-	public boolean logic maior (variavel b, int a){
-		if(b.valor > a){
+	public boolean logic_maior (variavel b, int a){
+		if(b.getVALOR()> a){
 			return logico = TRUE;	
 		}	
 		else{
 			return logico = FALSE;		
 		}
 	}
-	public boolean logic menor(variavel b,int a){
-		if(b.valor < a){
+	public boolean logic_menor(variavel b,int a){
+		if(b.getVALOR() < a){
 			return logico = TRUE;	
 		}	
 		else{
--- fonte/Lance.java
+++ fonte/Lance.java
@@ -2,13 +2,13 @@ class Lance extends Fluxo extends Operador{
 
 	public boolean while(operador a,variavel b, int c){
 			if(a.CondGet()==0){
-				return public boolean logic igual(variavel b, int c );
+				return logic_igual(variavel b, int c );
 			}
 			if(a.CondGet()==1){
-				return public boolean logic maior(variavel b, int c);
+				return logic_maior(variavel b, int c);
 			}
 			if(a.CondGet()==-1){
-				return public boolean logic menor(variavel b, int c);
+				return logic_menor(variavel b, int c);
 			}
 	}
 
